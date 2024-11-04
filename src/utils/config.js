a5460c79acbf63654dcf45b71fda74ed9ca4a30f
e.mamekov <mamekov-e@mail.ru>
2024-11-04 05:53:41 +0500

    Update config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8080/api/v1"
+export const BASE_URL = "http://13.61.34.63:8080/api/v1"
 export const TELEGRAM_BOT_URL = "https://t.me/site_health_tracker_bot"
